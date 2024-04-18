fab5a8953db52d69cfe23e800a10dd2235324a15
floconno <floconno@asu.edu>
2024-04-17 18:14:31 -0700

    added inquirer list

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const inquirer = require('inquirer');
 // Import and require mysql2
 const mysql = require('mysql2');
 
@@ -21,3 +22,27 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employees_db database.`)
 );
+
+function start() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'directory',
+            message: 'What would you like to do?',
+            choices: [
+                "View All Employees",
+                "Add Employee",
+                "Update Employee Role",
+                "view All Roles",
+                "Add Role",
+                "View All Departments",
+                "Add Department",
+                "Quit"
+            ]
+        },
+    ])
+
+
+}
+
+start();
